e4c451159cfbe1433c352a647a82cec69d1202fe
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-08-13 12:43:56 +0530

    Use React 18 createRoot

--- src/opend_frontend/src/index.jsx
+++ src/opend_frontend/src/index.jsx
@@ -1,13 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./components/App";
 import { Principal } from "@dfinity/principal";
 
 const CURRENT_USER_ID = Principal.fromText("2vxsx-fae");
 export default CURRENT_USER_ID;
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 const init = async () => {
-  ReactDOM.render(<App />, document.getElementById("root"));
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
 };
 
 init();
